863eca98726e4bb31dd198dc30c63ed246800cd3
Akkadius <akkadius1@gmail.com>
2015-12-09 19:40:26 -0600

    Update server start scripts

--- t_start_server.bat
+++ t_start_server.bat
@@ -3,5 +3,5 @@ cls
 echo *** CLEARING OLD LOGS ***
 del "logs/zone/" /q
 shared_memory.exe
-start perl win_server_launcher.pl zones="60" zone_background_start loginserver kill_all_on_start
+start perl win_server_launcher.pl zones="30" zone_background_start kill_all_on_start
 exit
