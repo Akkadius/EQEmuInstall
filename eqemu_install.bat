dcaea51790176cdec06c7add86df0192120f58db
Akkadius <akkadius1@gmail.com>
2016-08-25 17:15:57 -0500

    Update Windows install script

--- eqemu_install.bat
+++ eqemu_install.bat
@@ -82,10 +82,10 @@ IF NOT EXIST "C:\Program Files\MariaDB 10.0" (
 )
 
 :GET_EQEMU_UPDATE
-IF NOT EXIST "eqemu_update.pl" (
-	C:\Perl\bin\perl.exe -MLWP::UserAgent -e "require LWP::UserAgent;  my $ua = LWP::UserAgent->new; $ua->timeout(10); $ua->env_proxy; my $response = $ua->get('https://raw.githubusercontent.com/EQEmu/Server/master/utils/scripts/eqemu_update.pl'); if ($response->is_success){ open(FILE, '> eqemu_update.pl'); print FILE $response->decoded_content; close(FILE); }
+IF NOT EXIST "eqemu_server.pl" (
+	C:\Perl\bin\perl.exe -MLWP::UserAgent -e "require LWP::UserAgent;  my $ua = LWP::UserAgent->new; $ua->timeout(10); $ua->env_proxy; my $response = $ua->get('https://raw.githubusercontent.com/EQEmu/Server/master/utils/scripts/eqemu_server.pl'); if ($response->is_success){ open(FILE, '> eqemu_server.pl'); print FILE $response->decoded_content; close(FILE); }
 )
-IF NOT EXIST "eqemu_update.pl" GOTO GET_EQEMU_UPDATE
+IF NOT EXIST "eqemu_server.pl" GOTO GET_EQEMU_UPDATE
 
 :GET_EQEMU_CONFIG
 IF NOT EXIST "eqemu_config.xml" (
@@ -98,7 +98,7 @@ IF EXIST "vcredist_x86.exe" (
 	del vcredist_x86.exe
 )
 
-C:\Perl\bin\perl.exe eqemu_update.pl installer
+C:\Perl\bin\perl.exe eqemu_server.pl installer
 
 pause
 
