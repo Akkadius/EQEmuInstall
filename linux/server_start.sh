e14c66486b0a20b7d4c64b67bb3e8b3976bff4f4
Akkadius <akkadius1@gmail.com>
2016-08-25 00:13:12 -0500

    Add shared memory to start scripts

--- linux/server_start.sh
+++ linux/server_start.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+./shared_memory
 perl server_launcher.pl zones="30" silent_launcher &
 echo "Server started - use server_status.sh to check server status"
--- linux/server_start_dev.sh
+++ linux/server_start_dev.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+./shared_memory
 perl server_launcher.pl zones="30" &
 echo "Server started - use server_status.sh to check server status"  
