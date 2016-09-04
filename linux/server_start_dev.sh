95e8f526ce7a3c45bc83478b9e21b5468ad1fbd7
Akkadius <akkadius1@gmail.com>
2016-09-04 16:50:55 -0500

    Update server_start_dev.sh

--- linux/server_start_dev.sh
+++ linux/server_start_dev.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 ./shared_memory
-perl server_launcher.pl zones="10" &
+perl server_launcher.pl zones="10" no_status_update & 
 echo "Server started - use server_status.sh to check server status"  
